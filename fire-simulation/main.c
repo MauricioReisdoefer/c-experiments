ff11daa99198d344bd385cc4602eaeebcae30291
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2026-02-22 08:28:21 -0300

    feat: GetNextLayer

--- fire-simulation/main.c
+++ fire-simulation/main.c
@@ -2,10 +2,31 @@
 #include <stdio.h>
 #include <Windows.h>
 
+typedef struct Vector2
+{
+    int x;
+    int y;
+} Vector2;
+
 int main()
 {
     printf("main");
     Sleep(3000);
     system("cls");
     printf("main 2");
+}
+
+Vector2 GetNextLayer(Vector2 current)
+{
+    Vector2 result = (Vector2){current.x * rand() % 3, current.y * rand() % 3};
+    result.x = Max(0, result.x);
+    result.y = Max(0, result.y);
+    return result;
+}
+
+int Max(int min, int value)
+{
+    if (min < value)
+        return value;
+    return min;
 }
